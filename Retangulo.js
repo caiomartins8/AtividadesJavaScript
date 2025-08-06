090ff26605c3f468f702e5bf4eeec526660a80f7
caiomartins8 <caiom7436@gmail.com>
2025-08-06 14:47:51 -0300

    correcao triangulo para retangulo

---
+++ Retangulo.js
@@ -0,0 +1,7 @@
+let altura = 6;
+let base = 4;
+
+
+area = altura*base;
+
+console.log("A área total é de :", area , "cm2" )
--- Triangulo.js
+++
@@ -1,7 +0,0 @@
-let altura = 6;
-let base = 4;
-
-
-area = (altura*base)/2;
-
-console.log("A área total é de :", area )
